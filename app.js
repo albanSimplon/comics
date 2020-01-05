ebce08e4e1e6f7d153d60fd136ff976d411a1f48
Alban <a.veux@laposte.net>
2020-01-05 09:56:46 +0100

    recup premiere info

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-//https://www.superheroapi.com/api.php/10157823447012103/search/batman
+const tab = []; 
 
-// const getTodosAsync = async function () {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-//     if (response.ok) {
-//         const jsonData = await response.json();
-//         console.log(jsonData);
-//     } else {
-//         console.error('server response : ' + response.status);
-//     }
-// }
-
-// getTodosAsync();
-
-const getTodosAsync = async function () {
+const recherche = async function (personnage) {
     try {
-        const response = await fetch('https://www.superheroapi.com/api.php/10157823447012103/search/batman');
+        const response = await fetch('https://www.superheroapi.com/api.php/10157823447012103/search/'+ personnage);
+        
         if (response.ok) {
             const jsonData = await response.json();
             console.log(jsonData);
+            if (jsonData.response === 'success' && jsonData.results.length > 0 ) {
+                tab.push(...jsonData.results);
+                let info = "";
+                tab.forEach((u)=>{
+                    
+                    info += '<div class="container"><div class="row">';
+                    info += '<div class="col-md-6"><img class="img-fluid" src="'+u.image.url+'" alt="'+u.name+'"></div>';
+                    info += '<div class="col-md-6"><h1 class="text-center">'+u.name+'</h1>';
+                    for (let [key, value] of Object.entries(u.appearance)) {
+                        info += '<p>'+(`${key}: ${value}`)+'</p>';
+                     }
+                     info += '</div>'
+
+                    info += '</div></div>';
+
+
+
+
+
+                })
+                document.getElementById('data').innerHTML = info;
+
+            } else {
+                console.log('je n\'ai pas trouvé de personnage');
+            }
         } else {
             console.error('server response : ' + response.status);
         }
     } catch (error) {
         console.error(error);
     }
+    
 }
 
-getTodosAsync();
+recherche('superman');
 
