d7fd047873c86e85d98dba70e85ff73f297446a3
Alban <a.veux@laposte.net>
2020-01-06 20:45:47 +0100

    premiere version graphique

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const tab = []; 
+let i = 0;
 
 const recherche = async function (personnage) {
     try {
@@ -6,16 +7,23 @@ const recherche = async function (personnage) {
         
         if (response.ok) {
             const jsonData = await response.json();
-            console.log(jsonData);
+            
             if (jsonData.response === 'success' && jsonData.results.length > 0 ) {
                 tab.push(...jsonData.results);
                 let info = "";
+                console.log(jsonData);
+                
                 tab.forEach((u)=>{
                     
                     info += '<div class="container"><div class="row">';
                     info += '<div class="col-md-6"><img class="img-fluid" src="'+u.image.url+'" alt="'+u.name+'"></div>';
                     info += '<div class="col-md-6"><h1 class="text-center">'+u.name+'</h1>';
-                    info += '<h3 class="text-center">'+u.biography.alignment+'</h3>';
+                    if (u.biography.alignment === "good") {
+                        info += '<h3 class="text-center text-success">'+u.biography.alignment+'</h3>';
+                    } else {
+                        info += '<h3 class="text-center text-danger">'+u.biography.alignment+'</h3>';
+                    }
+                    
                     
                     for (let [key, value] of Object.entries(u.biography)) {
                         if (key === "full-name"){
@@ -25,17 +33,27 @@ const recherche = async function (personnage) {
                     for (let [key, value] of Object.entries(u.appearance)) {
                         info += '<p>'+(`${key}: ${value}`)+'</p>';
                      }
-                     info += '</div>'
+
+                     info += "<canvas id="+u.id+"></canvas>";
+                
+                     
+                   
+                     info += '</div>';
 
                     info += '</div></div>';
 
 
-
-
-
+                   
+                    
+                     console.log(u);
+                     
+                     
                 })
                 document.getElementById('data').innerHTML = info;
-
+                
+                
+                
+                
             } else {
                 console.log('je n\'ai pas trouvé de personnage');
             }
@@ -48,5 +66,62 @@ const recherche = async function (personnage) {
     
 }
 
-recherche('superman');
+recherche('batman').then( function(){
+    if(document.body.contains(document.getElementById('69'))){
+        alert('Element exists!');
+        console.log(tab);
+        for (i = 0; i < tab.length; i++)
+        {
+            console.log(tab[i].id);
+            new Chart(document.getElementById(tab[i].id), {
+    type: 'bar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+            
+        }
+        
+    } else{
+        alert('Element does not exist!');
+    }
+});
+
+
+// new Chart(document.getElementById("69"), {
+//     type: 'bar',
+//     data: {
+//       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+//       datasets: [
+//         {
+//           label: "Population (millions)",
+//           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+//           data: [2478,5267,734,784,433]
+//         }
+//       ]
+//     },
+//     options: {
+//       legend: { display: false },
+//       title: {
+//         display: true,
+//         text: 'Predicted world population (millions) in 2050'
+//       }
+//     }
+// });
+
+
 
